00a5e67d6fbcec060c22f306472ede3ac040cb02
Trishna <trishna.patil13@gmail.com>
2024-04-16 19:06:58 -0400

    fixed minor bugs

--- src/components/UserFinder.js
+++ src/components/UserFinder.js
@@ -1,33 +1,31 @@
 import { Fragment, useState, useEffect, Component } from "react";
-
 import Users from "./Users";
 import classes from "./UserFinder.module.css";
-
-const DUMMY_USERS = [
-  { id: "u1", name: "Max" },
-  { id: "u2", name: "Manuel" },
-  { id: "u3", name: "Julie" },
-];
+import UsersContext from "../store/users-context";
 
 class UserFinder extends Component {
+  static contextType = UsersContext; //can only use this once per component!
+
   constructor() {
     super();
     this.state = {
-      filteredUsers: DUMMY_USERS,
+      filteredUsers: [],
       searchTerm: "",
     };
   }
 
   componentDidMount() {
+    console.log(this.contextType);
     //runs only once at the start
     //can send http requests and set the initial state
+    this.setState({ filteredUsers: this.context.users });
   }
 
   componentDidUpdate(prevProps, prevState) {
     //runs everytime the components updates, we have access to the prev props and state
     if (prevState.searchTerm !== this.state.searchTerm) {
       this.setState({
-        filteredUsers: DUMMY_USERS.filter((user) =>
+        filteredUsers: this.context.users.filter((user) =>
           user.name.includes(this.state.searchTerm)
         ),
       });
